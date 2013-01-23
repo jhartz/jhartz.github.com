30c457bd3cd6a2575903b74af7cf017f85079938
Jake Hartz <jhartz@outlook.com>
2013-01-23 16:42:39 -0500

    Fixed issue with leading zeros; added special zero case

--- math/repdec.js
+++ math/repdec.js
@@ -22,6 +22,7 @@ var repdec = {
         frac_simp: "repdec_frac_simp",  // Element to show if a simplified fraction is available (optional)
         frac_simp_top: "repdec_frac_simp_top",  // Element in which to display the numerator of a simplified fraction (optional)
         frac_simp_bottom: "repdec_frac_simp_bottom",  // Element in which to display the denominator of a simplified fraction (optional)
+        frac_zero: "repdec_frac_zero",  // Element to display if the fraction is equal to zero (optional)
         frac_one: "repdec_frac_one"  // Element to display if the fraction is equal to one (optional)
     },
     required: ["a", "b", "dec", "frac_top", "frac_bottom"],
@@ -37,6 +38,15 @@ var repdec = {
         throw msg;
     },
     
+    prettyerr: function (msg) {
+        if (repdec.elems.dec) repdec.elems.dec.innerHTML = msg;
+        repdec.elems.frac_top.innerHTML = "0";
+        repdec.elems.frac_bottom.innerHTML = "0";
+        if (repdec.elems.frac_simp) repdec.elems.frac_simp.style.display = "none";
+        if (repdec.elems.frac_zero) repdec.elems.frac_zero.style.display = "none";
+        if (repdec.elems.frac_one) repdec.elems.frac_one.style.display = "none";
+    },
+    
     reduce: function (top, bottom) {
         var gcd = function gcd(a, b) {
             return b ? gcd(b, a % b) : a;
@@ -45,39 +55,53 @@ var repdec = {
         return [top/gcd, bottom/gcd];
     },
     
+    addzeroes: function (orig_str, new_str) {
+        if (new_str == "0") new_str = "";
+        if (orig_str.charAt(0) == "0") {
+            var place = 0;
+            while (place !== false) {
+                if (orig_str.charAt(place) == "0") {
+                    new_str = "0" + new_str;
+                    place++;
+                } else {
+                    place = false;
+                }
+            }
+        }
+        return new_str;
+    },
+    
     update: function () {
         var a = repdec.elems.a.value ? parseInt(repdec.elems.a.value, 10) : null;
         var b = repdec.elems.b.value ? parseInt(repdec.elems.b.value, 10) : null;
         if (isNaN(a) || isNaN(b) || b === null || (a && a < 0) || (b && b < 0)) {
-            if (repdec.elems.dec) repdec.elems.dec.innerHTML = "Not a number";
-            repdec.elems.frac_top.innerHTML = "0";
-            repdec.elems.frac_bottom.innerHTML = "0";
-            if (repdec.elems.frac_one) repdec.elems.frac_one.style.display = "none";
-            if (repdec.elems.frac_simp) repdec.elems.frac_simp.style.display = "none";
+            repdec.prettyerr("Not a number");
         } else {
             var str_a = a === null ? "" : a.toString(),
                 str_b = b === null ? "" : b.toString();
             if (str_a.indexOf("e") != -1 || str_b.indexOf("e") != -1 || str_a.length > 15 || str_b.length > 15) {
                 // We don't want any weirdness with big numbers that are represented oddly in float-world
-                if (repdec.elems.dec) repdec.elems.dec.innerHTML = "Number too large";
-                repdec.elems.frac_top.innerHTML = "0";
-                repdec.elems.frac_bottom.innerHTML = "0";
-                if (repdec.elems.frac_one) repdec.elems.frac_one.style.display = "none";
-                if (repdec.elems.frac_simp) repdec.elems.frac_simp.style.display = "none";
+                repdec.prettyerr("Number too large");
             } else {
+                // Zeroes in front of a or b?
+                if (a !== null) str_a = repdec.addzeroes(repdec.elems.a.value, str_a);
+                str_b = repdec.addzeroes(repdec.elems.b.value, str_b);
                 repdec.elems.dec.innerHTML = '0.' + str_a + '<span style="border-top: 1px solid;">' + str_b + '</span> <em>or</em> 0.' + str_a + str_b + str_b + str_b + '...';
-                var top = b;
-                if (a !== null) {
-                    var new_a = Number(a.toString() + b.toString());
-                    top = new_a - a;
-                }
+                
+                var top = Number(str_a + str_b) - a;
                 var bottom = Number((new Array(str_b.length + 1)).join("9") + (new Array(str_a.length + 1)).join("0"));
                 repdec.elems.frac_top.innerHTML = top;
                 repdec.elems.frac_bottom.innerHTML = bottom;
-                if (top == bottom) {
-                    if (repdec.elems.frac_one) repdec.elems.frac_one.style.display = "block";
+                if (top == 0 && repdec.elems.frac_zero) {
                     if (repdec.elems.frac_simp) repdec.elems.frac_simp.style.display = "none";
+                    if (repdec.elems.frac_zero) repdec.elems.frac_zero.style.display = "block";
+                    if (repdec.elems.frac_one) repdec.elems.frac_one.style.display = "none";
+                } else if (top == bottom && repdec.elems.frac_one) {
+                    if (repdec.elems.frac_simp) repdec.elems.frac_simp.style.display = "none";
+                    if (repdec.elems.frac_zero) repdec.elems.frac_zero.style.display = "none";
+                    if (repdec.elems.frac_one) repdec.elems.frac_one.style.display = "block";
                 } else {
+                    if (repdec.elems.frac_zero) repdec.elems.frac_zero.style.display = "none";
                     if (repdec.elems.frac_one) repdec.elems.frac_one.style.display = "none";
                     if (repdec.elems.frac_simp_top && repdec.elems.frac_simp_bottom) {
                         var reduced = repdec.reduce(top, bottom);
