0193fd3599b5a6b7bb29b1cf584c5f745102f263
Jake Hartz <jhartz@outlook.com>
2013-02-12 18:30:28 -0500

    Updated boosts spec

--- running/virtual/vr-boosts.js
+++ running/virtual/vr-boosts.js
@@ -6,35 +6,41 @@
 
 // NOTE: Update manifest.appcache when you update this file
 
+/*
+    "name": {
+        action: string,     // name of action
+        strength: number,   // higher strength = more of a boost, but fewer uses (pick a value BETWEEN 1 and 8)
+        addictive: boolean, // if true, then after the boost is used too much, the user becomes dependant on it and its effects are the opposite of "strength" (1/strength)
+        sound: string,      // url of sound to play while boost is being used
+        image: string       // url of image to show while boost is being used
+    }
+*/
+
 vr.options.boosts = {
     "magic": {
-        strength: 10,
-        uses: 4,
-        addictive: false,
+        action: "Use Magic",
+        strength: 7,
         sound: "boosts/magic.wav",
         image: "boosts/magic.png"
     },
     
     "coffee": {
-        strength: 8,
-        uses: 5,
-        addictive: true,  // if addictive, then after we use it too much, we become dependant on it and its effects are the opposite of "strength" (1/strength)
-        sound: null,
+        action: "Drink Coffee",
+        strength: 5,
+        addictive: true,
         image: "boosts/coffee.png"
     },
     
     "violin": {
-        strength: 5,
-        uses: 8,
-        addictive: false,
+        action: "Play Music",
+        strength: 3,
         sound: "boosts/violin.wav",
         image: "boosts/violin.png"
     },
     
     "syrup": {
-        strength: 5,
-        uses: 8,
-        addictive: false,
+        action: "Drink Syrup",
+        strength: 3,
         sound: "boosts/canada.wav",
         image: "boosts/canada.png"
     }
