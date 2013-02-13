0979fc0ef98278b35c64363c0f634fabd8252261
Jake Hartz <jhartz@outlook.com>
2013-02-12 22:02:45 -0500

    Realistic-ified strength values

--- running/virtual/vr-boosts.js
+++ running/virtual/vr-boosts.js
@@ -9,8 +9,8 @@
 /*
     "name": {
         action: string,     // name of action
-        strength: number,   // higher strength = more of a boost, but fewer uses (pick a value BETWEEN 1 and 8)
-        addictive: boolean, // if true, then after the boost is used too much, the user becomes dependant on it and its effects are the opposite of "strength" (1/strength)
+        strength: number,   // higher strength = more of a boost, but fewer uses (pick a value BETWEEN 0 and 10)
+        addictive: boolean, // if true, then after the boost is used too much, the user becomes dependent on it
         sound: string,      // url of sound to play while boost is being used
         image: string       // url of image to show while boost is being used
     }
@@ -19,28 +19,28 @@
 vr.options.boosts = {
     "magic": {
         action: "Use Magic",
-        strength: 7,
+        strength: 9,
         sound: "boosts/magic.wav",
         image: "boosts/magic.png"
     },
     
     "coffee": {
         action: "Drink Coffee",
-        strength: 5,
+        strength: 7,
         addictive: true,
         image: "boosts/coffee.png"
     },
     
     "violin": {
         action: "Play Music",
-        strength: 3,
+        strength: 5,
         sound: "boosts/violin.wav",
         image: "boosts/violin.png"
     },
     
     "syrup": {
         action: "Drink Syrup",
-        strength: 3,
+        strength: 5,
         sound: "boosts/canada.wav",
         image: "boosts/canada.png"
     }
