fcbd1f31b09889d93db09eb22a7df59809afec5d
Jake Hartz <jhartz@outlook.com>
2015-01-07 02:05:10 -0500

    Dat radical 2-space indent...

--- decorations/snow.js
+++ decorations/snow.js
@@ -4,18 +4,18 @@
 // http://www.schillmania.com/projects/snowstorm/
 
 decorations.snow = function (isOnLoad) {
-    if (snowStorm.active) {
-        decorations.cookie("snow", "no", isOnLoad);
-        
-        document.getElementById("decorations_snow_toggle_start").style.display = "inline";
-        document.getElementById("decorations_snow_toggle_stop").style.display = "none";
-    } else {
-        decorations.cookie("snow", "yes", isOnLoad);
-        
-        document.getElementById("decorations_snow_toggle_start").style.display = "none";
-        document.getElementById("decorations_snow_toggle_stop").style.display = "inline";
-    }
-    snowStorm.toggleSnow();
+  if (snowStorm.active) {
+    decorations.cookie("snow", "no", isOnLoad);
+    
+    document.getElementById("decorations_snow_toggle_start").style.display = "inline";
+    document.getElementById("decorations_snow_toggle_stop").style.display = "none";
+  } else {
+    decorations.cookie("snow", "yes", isOnLoad);
+    
+    document.getElementById("decorations_snow_toggle_start").style.display = "none";
+    document.getElementById("decorations_snow_toggle_stop").style.display = "inline";
+  }
+  snowStorm.toggleSnow();
 };
 
 
@@ -25,32 +25,32 @@ snowStorm.className = "noprint"; // hide the snow when printing
 
 // This is almost exactly like the very end of snowstorm.js
 snowStorm.events.add(window, "load", function doStart() {
-    snowStorm.events.remove(window, "load", doStart);
-    
-    var doSnow = false;
-    if (decorations.defaults.snow) {
-        if (decorations.cookie("snow")) {
-            doSnow = decorations.cookie("snow") != "no";
-        } else {
-            // Same logic used in snowstorm.js
-            doSnow = !(navigator.userAgent.match(/mobile|opera m(ob|in)/i));
-        }
+  snowStorm.events.remove(window, "load", doStart);
+  
+  var doSnow = false;
+  if (decorations.defaults.snow) {
+    if (decorations.cookie("snow")) {
+      doSnow = decorations.cookie("snow") != "no";
     } else {
-        doSnow = decorations.cookie("snow") == "yes";
-        if (!decorations.cookie("snow")) {
-            // Show the "festive" message! (but only for the first 5 visits while the snow cookie is still unset)
-            var numtimes = Number(decorations.cookie("festive"));
-            if (isNaN(numtimes)) numtimes = 0;
-            decorations.cookie("festive", ++numtimes, true);
-            if (numtimes <= 5) {
-                document.getElementById("decorations_snow_festive_mood").style.display = "block";
-            }
-        }
+      // Same logic used in snowstorm.js
+      doSnow = !(navigator.userAgent.match(/mobile|opera m(ob|in)/i));
     }
-    
-    if (doSnow) {
-        decorations.snow(true);
-    } else {
-        document.getElementById("decorations_snow_toggle_start").style.display = "inline";
+  } else {
+    doSnow = decorations.cookie("snow") == "yes";
+    if (!decorations.cookie("snow")) {
+      // Show the "festive" message! (but only for the first 5 visits while the snow cookie is still unset)
+      var numtimes = Number(decorations.cookie("festive"));
+      if (isNaN(numtimes)) numtimes = 0;
+      decorations.cookie("festive", ++numtimes, true);
+      if (numtimes <= 5) {
+          document.getElementById("decorations_snow_festive_mood").style.display = "block";
+      }
     }
+  }
+  
+  if (doSnow) {
+    decorations.snow(true);
+  } else {
+    document.getElementById("decorations_snow_toggle_start").style.display = "inline";
+  }
 }, false);
